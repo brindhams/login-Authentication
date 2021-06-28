1d5b8db772e81d2493a198e332fab3cd5770a9ee
brindhams <85442521+brindhams@users.noreply.github.com>
2021-06-28 11:45:54 +0530

    Add files via upload

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
+import uuid
 from models import db
 
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer(),  primary_key=True)
+    id = db.Column(db.Integer(),  primary_key=True, default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(128))
     mail_id = db.Column(db.String(128))
     password = db.Column(db.String(128))
@@ -20,7 +21,7 @@ class User(db.Model):
 
 
     @staticmethod
-    def create_user(name,mail_id,password):
+    def new_user(name,mail_id,password):
         user=User(
             name=name,
             mail_id=mail_id,
@@ -36,3 +37,17 @@ class User(db.Model):
     def get_users():
         users = User.query.all()
         return users
+
+class Session(db.Model):
+    __tablename__='Session'
+    
+    Session_id = db.Column(db.String(),  primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(128))
+    mail_id = db.Column(db.String(128))
+    password = db.Column(db.String(128))
+    
+
+    @staticmethod
+    def create_session(Session):
+        ses = Session
+        return ses
